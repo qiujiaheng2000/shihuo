3710f26f3d140101913ec504450fbbebe59b019c
lishuai <lishuai@dongqiudi.com>
2016-11-26 21:34:58 +0800

    aa

--- shihuo/app/src/main/java/com/shihuo/shihuo/Activities/ChangePasswordActivity.java
+++ shihuo/app/src/main/java/com/shihuo/shihuo/Activities/ChangePasswordActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
